538213e64a0d9c4065a751d3e47268e19905d7fc
Kelly Bazella <kelly.bazella@gmail.com>
2020-07-26 14:39:27 -0500

    delete button functional

--- app.js
+++ app.js
@@ -11,12 +11,41 @@ loadEventListeners();
 // Load all event Listeners
 function loadEventListeners(){
     form.addEventListener('submit', addTask)
+    taskList.addEventListener('click', removeTask)
 };
 
 function addTask(e){
     if(taskInput.value === ''){
         alert("Please Add Task")
-        console.log("task please")
     }
+
+    //create LI element
+    const li=document.createElement('li');
+    li.className='collection-item';
+    //create textnode and append to LI
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link
+    const link=document.createElement('a');
+    //add class
+    link.className='delete-item secondary-content'
+    //add icon
+    link.innerHTML=(`<i class="fa fa-remove"></i>`)
+    //append link to li
+    li.appendChild(link);
+    //append li to task list defined at top
+    taskList.appendChild(li);
+    //clear the input value
+    taskInput.value="";
+    //make sure page doesn't auto refresh
     e.preventDefault();
+};
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure')){
+            e.target.parentElement.parentElement.remove()
+        }
+    
+    }
+        
 }
